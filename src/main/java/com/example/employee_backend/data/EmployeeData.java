4d6fb25fdcaf29d0c89a9d219f0966132211868f
mevshnu <vishnusivan1000@gmail.com>
2022-12-06 10:16:54 +0530

    polo

--- src/main/java/com/example/employee_backend/data/EmployeeData.java
+++ src/main/java/com/example/employee_backend/data/EmployeeData.java
@@ -1,37 +1,49 @@
 package com.example.employee_backend.data;
 
 
+import com.example.employee_backend.model.Employe;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeData {
-    @GetMapping("/")
+    @PostMapping("/")
     public String Home()
     {
         return "Welcome to Homepage";
     }
-    @GetMapping("/search")
+    @PostMapping("/search")
     public String Search()
     {
         return "Search";
     }
-    @GetMapping("/add")
-    public String Add()
+    @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
+    public String Add(@RequestBody Employe e)
     {
-        return "add";
+        System.out.println(e.getCode());
+        System.out.println(e.getName().toString());
+        System.out.println(e.getDesignation().toString());
+        System.out.println(e.getSalary());
+        System.out.println(e.getCompanyName().toString());
+        System.out.println(e.getMobileNo().toString());
+        System.out.println(e.getUsername().toString());
+        System.out.println(e.getPassword().toString());
+
+        return "added successfully";
     }
     @GetMapping("/view")
     public String View()
     {
         return "view";
     }
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     public String Delete()
     {
         return "delete";
     }
-    @GetMapping("/edit")
+    @PostMapping("/edit")
     public String Edit()
     {
         return "edit";
